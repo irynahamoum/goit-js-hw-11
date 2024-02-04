7bba2962506f0af27f0665e316ec1d8b9a03f3cf
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-02-04 20:34:26 +0100

    html+styles

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.form');
+const galleryGrid = document.querySelector('.images-section');
+
+form.addEventListener(submit, () => {});
