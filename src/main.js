a2163e5ee0d233d36cedb7f82ffabf96a9ea52f8
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-02-14 13:27:10 +0100

    completed

--- src/main.js
+++ src/main.js
@@ -4,7 +4,90 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form');
-const input = document.querySelector('#search');
-const galleryGrid = document.querySelector('.images-section');
+const galleryGrid = document.querySelector('.gallery-grid');
 
-form.addEventListener(submit, () => {});
+const BASE_URL = 'https://pixabay.com/api/';
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const inputValue = event.target.elements.query.value.trim();
+  if (!inputValue) {
+    iziToast.error({
+      message: 'Search field is empty',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  getImagesByInputValue(inputValue)
+    .then(data => {
+      if (data?.hits?.length > 0) {
+        createMarkupByHits(data.hits);
+        return;
+      }
+      throw Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    })
+    .catch(error => {
+      iziToast.info({
+        message:
+          typeof error === 'string' ? error : 'Something went wrong, sorry!',
+        position: 'topRight',
+      });
+    });
+
+  event.target.reset();
+}
+
+function getImagesByInputValue(q) {
+  const paramsStrQuery = new URLSearchParams({
+    key: '42157668-d969611c6fdd34526589fe987',
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safeSearch: true,
+  });
+
+  const PARAMS = `?${paramsStrQuery}`;
+  const url = BASE_URL + PARAMS;
+
+  return fetch(url).then(response => response.json());
+}
+
+function createMarkupByHits(hits) {
+  galleryGrid.innerHTML = '';
+
+  const galleryItem = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a href="${largeImageURL}">
+      <img src="${webformatURL}" alt="${tags}" class="gallery-card">
+      <p><b>Likes:</b> ${likes}</p>
+      <p><b>Views:</b> ${views}</p>
+      <p><b>Comments:</b> ${comments}}</p>
+      <p><b>Downloads:</b> ${downloads}</p>
+      </a>`;
+      }
+    )
+    .join('');
+
+  galleryGrid.insertAdjacentHTML('beforeend', galleryItem);
+
+  let gallery = new SimpleLightbox('.gallery-grid a', {
+    captionsData: 'alt',
+  });
+
+  gallery.refresh();
+}
